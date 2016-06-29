d4c7536e966bc7902d965e6bd58f54d8baacb3df
edawg11 <eliot.smith1185@gmail.com>
2016-06-29 14:10:27 -0400

    react

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 router.get('/home', function(req, res, next) {
